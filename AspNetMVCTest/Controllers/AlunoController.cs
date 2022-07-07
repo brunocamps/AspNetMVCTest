31d884174c839c276646b3b2157e044231cda7b2
Bruno Campos <bruno.mcamps@outlook.com>
2022-07-07 06:05:30 -0300

    Added validation

--- AspNetMVCTest/Controllers/AlunoController.cs
+++ AspNetMVCTest/Controllers/AlunoController.cs
@@ -9,10 +9,21 @@ namespace AspNetMVCTest.Controllers
 {
     public class AlunoController : Controller
     {
+        [HttpGet]
+        [Route("novo-aluno")]
+        // route
+        public ActionResult NovoAluno()
+        {
+            return View();
+        }
+
+        [HttpPost]
         [Route("novo-aluno")]
         public ActionResult NovoAluno(Aluno aluno)
         {
             if (!ModelState.IsValid) return View(aluno);
+
+            // alguma regra de negocio + Salvar no banco
 
             return View(aluno);
 
